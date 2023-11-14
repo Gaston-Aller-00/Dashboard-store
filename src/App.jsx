3c2c288249ea7e61c2824564149825c8fbfb8462
Gaston <gastonallerdev@gmail.com>
2023-11-13 23:34:15 -0300

    end day 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import SideBar from "./components/shared/SideBar"
+import {RiMenu3Fill} from "react-icons/ri";
+//componentes
+import SideBar from "./components/shared/SideBar";
 function App() {
-
   return (
     <>
       <div className="bg-[#262837] w-full min-h-screen">
-        <SideBar/>
+        <SideBar />
+        {/* menu movil */}
+        <nav className="bg-[#1F1D2B] lg:hidden">
+          <button><RiMenu3Fill/></button>
+        </nav>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
