edbe5b1d3b1b692a4ba3ff51d136c987bdcf3a08
Gaston <gastonallerdev@gmail.com>
2023-11-14 21:33:17 -0300

    add states and movil menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
-import {RiMenu3Fill} from "react-icons/ri";
+import { useState } from "react";
+import {
+  RiAddLine,
+  RiUser3Line,
+  RiMenu3Fill,
+  RiPieChartLine,
+  RiCloseLine,
+} from "react-icons/ri";
 //componentes
 import SideBar from "./components/shared/SideBar";
 function App() {
+  // states
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
+
+  // functions
+  // change visivility of menu
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <>
       <div className="bg-[#262837] w-full min-h-screen">
-        <SideBar />
+        <SideBar showMenu={showMenu} />
         {/* menu movil */}
-        <nav className="bg-[#1F1D2B] lg:hidden">
-          <button><RiMenu3Fill/></button>
+        <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl ">
+          <button className="p-2">
+            <RiUser3Line />
+          </button>
+          <button className="p-2">
+            <RiAddLine />
+          </button>
+          <button className="p-2">
+            <RiPieChartLine />
+          </button>
+          <button onClick={toggleMenu} className="text-white  p-2 ">
+            {showMenu ?  <RiCloseLine/> : <RiMenu3Fill/>}
+           
+          </button>
         </nav>
       </div>
     </>
