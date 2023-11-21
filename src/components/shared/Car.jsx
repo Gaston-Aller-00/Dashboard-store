febf99d74f92bfe9244e212429ae8c5bc09b6681
Gaston <gastonallerdev@gmail.com>
2023-11-21 11:23:13 -0300

    finish car

--- src/components/shared/Car.jsx
+++ src/components/shared/Car.jsx
@@ -5,12 +5,12 @@ const Card = (props) => {
   const { showOrder, setShowOrder } = props;
   return (
     <div
-      className={`lg:col-span-2 fixed top-0 bg-[#1F1D2B] w-full lg:w-96 lg:right-0 h-full transition-all z-50 ${
+      className={`lg:col-span-2 fixed top-0 bg-[#1F1D2B] w-full lg:w-96 lg:right-0 h-full transition-all z-50 rounded-b-2xl rounded-t-2xl  ${
         showOrder ? "right-0" : "-right-full"
       }`}
     >
       {/* Orders */}
-      <div className="relative pt-16 lg:pt-8 text-gray-300 p-8 h-full">
+      <div className="relative pt-16 lg:pt-8 text-gray-300 p-8 h-full ">
         <RiCloseLine
           onClick={() => setShowOrder(false)}
           className="lg:hidden absolute left-4 top-4 p-3 box-content text-gray-300 bg-[#262837] rounded-full text-xl"
@@ -30,7 +30,7 @@ const Card = (props) => {
         </div>
         {/* Car */}
         <div>
-          <div className="grid grid-cols-6 mb-4 p-4">
+          <div className="grid grid-cols-6 mb-4 p-4 rounded-2xl">
             <h5 className="col-span-4">Item</h5>
             <h5>Qty</h5>
             <h5>Price</h5>
@@ -184,7 +184,7 @@ const Card = (props) => {
           </div>
         </div>
         {/* Submit payment */}
-        <div className="bg-[#262837] absolute w-full bottom-0 left-0 p-4">
+        <div className="bg-[#262837] absolute w-full bottom-0 left-0 p-4 ">
           <div className="flex items-center justify-between mb-4">
             <span className="text-gray-400">Discount</span>
             <span>$0</span>
